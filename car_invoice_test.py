0df34a57a382615524e4cc3ec2f045717d15e92a
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-09-24 22:56:05 +0530

    get all details of user by user id

--- car_invoice_test.py
+++ car_invoice_test.py
@@ -1,13 +1,24 @@
+import pytest
 
-from main import InvoiceGenerator
-#import unittest
+from main import InvoiceGenerator, NegativeValueError
 
 
 class TestCalculator:
     def test_output(self):
-        check = InvoiceGenerator.calculate_fare(20,60)
+        check = InvoiceGenerator.calculate_fare(0,20,60)
         assert check.total_fare == 265
 
     def test_null(self):
-        check = InvoiceGenerator.calculate_fare(0.6,0)
+        check = InvoiceGenerator.calculate_fare(0,0.6,0)
         assert check.total_fare == 5
+
+    def test_exceptions(self):
+        with pytest.raises(NegativeValueError):
+            assert InvoiceGenerator.calculate_fare(0,-10,10)
+
+    def test_raises(self):
+        with pytest.raises(NegativeValueError) as execinfo:
+            InvoiceGenerator.calculate_fare(0, -10, 10)
+        # these asserts are identical; you can use either one
+        assert execinfo.value.args[0] == 'negative input'
+        #assert str(execinfo.value) == 'some info'
